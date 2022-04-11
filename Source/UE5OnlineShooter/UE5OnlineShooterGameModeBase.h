a3cd627fa55b17e375dc8a32682a605896b29d72
David Considine <DavidConsidine@users.noreply.github.com>
2022-04-11 13:35:09 +0100

    Create UE5 project

---
+++ Source/UE5OnlineShooter/UE5OnlineShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5OnlineShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5ONLINESHOOTER_API AUE5OnlineShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
