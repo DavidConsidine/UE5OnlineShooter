7ae7a65abce98964ea3347be2368342e9913e4e8
David Considine <DavidConsidine@users.noreply.github.com>
2022-04-14 13:15:10 +0100

    Implement Character movement.

--- Source/UE5OnlineShooter/Private/Character/BaseShooterCharacter.cpp
+++ Source/UE5OnlineShooter/Private/Character/BaseShooterCharacter.cpp
@@ -25,15 +25,50 @@ void ABaseShooterCharacter::BeginPlay()
 	
 }
 
+void ABaseShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+
+	PlayerInputComponent->BindAxis("Move Forward / Backward", this, &ThisClass::MoveForward);
+	PlayerInputComponent->BindAxis("Move Right / Left", this, &ThisClass::MoveRight);
+	PlayerInputComponent->BindAxis("Turn Right / Left Mouse", this, &ThisClass::Turn);
+	PlayerInputComponent->BindAxis("Look Up / Down Mouse", this, &ThisClass::LookUp);
+}
+
+void ABaseShooterCharacter::MoveForward(float Value)
+{
+	if (Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void ABaseShooterCharacter::MoveRight(float Value) 
+{
+	if (Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void ABaseShooterCharacter::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void ABaseShooterCharacter::LookUp(float Value)
+{
+	AddControllerPitchInput(Value);
+}
+
 void ABaseShooterCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
-
-void ABaseShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
